83a33f9381fb42968eee9e12c7c477b7c009eb26
Neil <30356902+NeilChaudhari@users.noreply.github.com>
2021-11-24 13:25:25 -0500

    Assigned hardware and set drive speed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstOpMode.java
@@ -1,15 +1,36 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
+
+@TeleOp(name="FirstOpMode", group="TeleOp")
 public class FirstOpMode extends OpMode {
+
+    DcMotor leftWheel;
+    DcMotor rightWheel;
+    double drivePower = 0.5;
     @Override
     public void init() {
+        leftWheel = hardwareMap.dcMotor.get("left_wheel");
+        rightWheel = hardwareMap.dcMotor.get("right_wheel");
 
+        rightWheel.setDirection(DcMotor.Direction.REVERSE);
+    }
+    @Override
+    public void start() {
+
+    }
+    @Override
+    public void loop() {
+        leftWheel.setPower(drivePower);
+        rightWheel.setPower(drivePower);
     }
 
     @Override
-    public void loop() {
+    public void stop() {
 
     }
 }
